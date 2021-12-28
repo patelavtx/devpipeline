cd8c8554f2cacfb7e8b7ac0da4b461bd61db544a
patelavtx <apatel@aviatrix.com>
2021-12-28 15:26:02 +0000

    add vars for vnet

---
+++ vnet/variables.tf
@@ -0,0 +1,21 @@
+
+variable "az_client_id" {
+  type        = string
+  description = "Client ID with permissions to create resources in Azure, use env variables"
+}
+
+variable "az_client_secret" {
+  type        = string
+  description = "Client secret with permissions to create resources in Azure, use env variables"
+}
+
+variable "az_subscription" {
+  type        = string
+  description = "Client ID subscription, use env variables"
+}
+
+variable "az_tenant" {
+  type        = string
+  description = "Client ID Azure AD tenant, use env variables"
+}
+
